b23288a158a6feac4dd76307f24fd1ed54bec659
Ezequiel Pereira López <eze2307@gmail.com>
2015-12-10 21:59:33 -0300

    Ported to WebKit

--- sugarizehelp.py
+++ sugarizehelp.py
@@ -15,23 +15,19 @@
 import os
 from gettext import gettext as _
 
-import gtk
-import gobject
+from gi.repository import Gtk
+from gi.repository import WebKit
 
-from sugar.activity import activity
-from sugar.graphics.toolbutton import ToolButton
+from sugar3.activity import activity
+from sugar3.activity.widgets import ActivityButton
+from sugar3.graphics.toolbarbox import ToolbarBox
+from sugar3.graphics.toolbarbox import ToolbarButton
+from sugar3.graphics.toolbutton import ToolButton
+from sugar3.activity.widgets import StopButton
 
-import hulahop
-hulahop.startup(os.path.join(activity.get_activity_root(), 'data/gecko'))
-
-#from hulahop.webview import WebView
-from browser import Browser
-import xpcom
-from xpcom.components import interfaces
 from viewtoolbar import ViewToolbar
-gobject.threads_init()
 
-HOME = os.path.join(activity.get_bundle_path(), 'sugarizehelp/Introduction')
+HOME = "file://"+os.path.join(activity.get_bundle_path(), 'sugarizehelp/Introduction')
 #HOME = "http://website.com/something.html"
 
 class SugarizeHelp(activity.Activity):
@@ -40,76 +36,75 @@ class SugarizeHelp(activity.Activity):
 
         self.props.max_participants = 1
 
-        self._web_view = Browser()
+        self._web_view = WebKit.WebView()
 
-        toolbox = activity.ActivityToolbox(self)
-        self.set_toolbox(toolbox)
+        toolbox = ToolbarBox()
+        self.set_toolbar_box(toolbox)
         toolbox.show()
 
-        toolbar = Toolbar(self._web_view)
-        toolbox.add_toolbar(_('Navegacion'), toolbar)
+        toolbar = toolbox.toolbar
         toolbar.show()
+
+        activity_button = ActivityButton(self)
+        toolbar.insert(activity_button, -1)
+
         viewtoolbar = ViewToolbar(self)
-        toolbox.add_toolbar(_('Ver'),viewtoolbar)
-        viewtoolbar.show()
-
-        self.set_canvas(self._web_view)
-        self._web_view.show()
-
-        toolbox.set_current_toolbar(1)
-
-        self._web_view.load_uri(HOME)
-
-class Toolbar(gtk.Toolbar):
-    def __init__(self, web_view):
-        gobject.GObject.__init__(self)
-
-        self._web_view = web_view
+        viewtoolbar_button = ToolbarButton(
+            page=viewtoolbar, icon_name='toolbar-view')
+        toolbar.insert(viewtoolbar_button, -1)
+        toolbar.show_all()
 
         self._back = ToolButton('go-previous-paired')
         self._back.set_tooltip(_('Back'))
         self._back.props.sensitive = False
         self._back.connect('clicked', self._go_back_cb)
-        self.insert(self._back, -1)
+        toolbar.insert(self._back, -1)
         self._back.show()
-
+        
         self._forward = ToolButton('go-next-paired')
         self._forward.set_tooltip(_('Forward'))
         self._forward.props.sensitive = False
         self._forward.connect('clicked', self._go_forward_cb)
-        self.insert(self._forward, -1)
+        toolbar.insert(self._forward, -1)
         self._forward.show()
-
+        
         home = ToolButton('go-home')
         home.set_tooltip(_('Home'))
         home.connect('clicked', self._go_home_cb)
-        self.insert(home, -1)
+        toolbar.insert(home, -1)
         home.show()
 
-        progress_listener = self._web_view.progress
-        progress_listener.connect('location-changed',
-                                  self._location_changed_cb)
-        progress_listener.connect('loading-stop', self._loading_stop_cb)
+        separator = Gtk.SeparatorToolItem()
+        separator.props.draw = False
+        separator.set_expand(True)
+        toolbar.insert(separator, -1)
 
-    def _location_changed_cb(self, progress_listener, uri):
-        self.update_navigation_buttons()
+        stopbtn = StopButton(self)
+        toolbar.insert(stopbtn, -1)
+        toolbar.show_all()
 
-    def _loading_stop_cb(self, progress_listener):
-        self.update_navigation_buttons()
+        self._web_view.connect('load-finished', self.update_navigation_buttons)
 
-    def update_navigation_buttons(self):
-        can_go_back = self._web_view.web_navigation.canGoBack
-        self._back.props.sensitive = can_go_back
+        self.set_canvas(self._web_view)
+        self._web_view.show()
 
-        can_go_forward = self._web_view.web_navigation.canGoForward
-        self._forward.props.sensitive = can_go_forward
-
-    def _go_back_cb(self, button):
-        self._web_view.web_navigation.goBack()
-    
-    def _go_forward_cb(self, button):
-        self._web_view.web_navigation.goForward()
-
-    def _go_home_cb(self, button):
         self._web_view.load_uri(HOME)
 
+    def _go_back_cb(self, button):
+        self._web_view.go_back()
+        pass
+    
+    def _go_forward_cb(self, button):
+        self._web_view.go_forward()
+        pass
+        
+    def _go_home_cb(self, button):
+        self._web_view.load_uri(HOME)
+        pass
+
+    def update_navigation_buttons(self, *args):
+        can_go_back = self._web_view.can_go_back()
+        self._back.props.sensitive = can_go_back
+
+        can_go_forward = self._web_view.can_go_forward()
+        self._forward.props.sensitive = can_go_forward
